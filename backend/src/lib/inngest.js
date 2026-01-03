47ec3abe1d3a50b96a0c8858fb68f82c9a9ce96d
Kashvi-M <kashvism100@gmail.com>
2026-01-03 21:41:57 +0530

    importing functions

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -8,34 +8,34 @@ const syncUser = inngest.createFunction(
     {id:"sync-user"},
     {event:"clerk/user.created"},
     async ({event}) => {
-        await connectDB()
+        await connectDB();
 
-        const {id,email_addresses, first_name,last_name,image_url} = event.data
+        const {id,email_addresses, first_name,last_name,image_url} = event.data;
 
         const newUser = {
             clerkId:id,
             email:email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""}`,
-            profileImage:image_url
-        }
+            profileImage: image_url,
+        };
 
-        await User.create(newUser)
+        await User.create(newUser);
 
         //todo: do smth else
     }
-)
+);
 
 const deleteUserFromDB = inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async ({event}) => {
-        await connectDB()
+    {id: "delete-user-from-db"},
+    {event: "clerk/user.deleted"},
+    async ({ event }) => {
+        await connectDB();
 
-        const {id} = event.data
+        const { id } = event.data;
             await User.deleteOne({ clerkId: id });
         
             // todo: do smth else
     }
-)
+);
 
 export const functions = [syncUser, deleteUserFromDB];
