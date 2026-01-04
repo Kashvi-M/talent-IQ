4a63aa0067323a5a656bcc7f4a7072a9f0dc042e
Kashvi-M <kashvism100@gmail.com>
2026-01-04 14:26:02 +0530

    importing function and updating api

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -16,15 +16,17 @@ export const upsertStreamUser = async(userData) => {
         console.log("Stream user upserted successfully:", userData);
     } catch (error) {
         console.error("Error upserting Stream user:", error);
+        throw error;
     }
 };
 
 export const deleteStreamUser = async(userId) => {
     try {
-        await chatClient.deleteUser([userId]);
+        await chatClient.deleteUser(userId);
         console.log("Stream user deleted successfully:", userId);
     } catch (error) {
         console.error("Error deleting Stream user:", error);
+        throw error;
     }
 };
 //todo other methods
