4db4903a4bdde1587c0ab770a257c0952f32c354
Kashvi-M <kashvism100@gmail.com>
2026-01-04 12:05:37 +0530

    auth and stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq"});
 
@@ -21,7 +22,11 @@ const syncUser = inngest.createFunction(
 
         await User.create(newUser);
 
-        //todo: do smth else
+       await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.profileImage,
+       });
     }
 );
 
@@ -32,9 +37,9 @@ const deleteUserFromDB = inngest.createFunction(
         await connectDB();
 
         const { id } = event.data;
-            await User.deleteOne({ clerkId: id });
-        
-            // todo: do smth else
+        await User.deleteOne({ clerkId: id });
+
+        await deleteStreamUser(id.toString());
     }
 );
 
